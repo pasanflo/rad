ea94a6082e998bd57a4899cf25842e2f80303805
pasanflo <pablosanchezflores@gmail.com>
2014-04-09 10:43:33 +0200

    Subo los progresos en clase

--- PArticulo/PArticulo/Main.cs
+++ PArticulo/PArticulo/Main.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Data;
-using System.DateTime;
 using MySql;
 using MySql.Data.MySqlClient;
 
@@ -18,31 +17,36 @@ namespace PArticulo
 			"Password=sistemas";
 			
 			string command = "SELECT * FROM articulo";
-			
-			string change = "UPDATE articulo SET nombre={1} where id=1", DateTime.Today; //Update con la fecha del día de hoy
-			
+	
 			MySqlConnection mySqlConnection = new MySqlConnection(connectionString); //Crear conexión a bd
 			
 			mySqlConnection.Open(); //Abrimos la conexión
 			
 			MySqlCommand mySqlCommand = mySqlConnection.CreateCommand(); //Creamos comando SQL
-			mySqlCommand.CommandText = change; //Cambiamos el texto del comando SQL por command.
+			mySqlCommand.CommandText = command; //Cambiamos el texto del comando SQL por command.
 			
 			MySqlCommand mySqlCommandChange = mySqlConnection.CreateCommand(); //Creamos comando SQL
-			mySqlCommand.CommandText = change; //Cambiamos el texto del comando SQL por command.
-
-			MySqlDataReader mySqlDataReader; //Creamos un DataReader
-			mySqlDataReader = mySqlCommand.ExecuteReader(); // Nos devuelve un mySqlDataReader, lector de datos
+			mySqlCommandChange.CommandText = "UPDATE articulo SET nombre=@nombre WHERE id=1"; //Cambiamos el texto del comando SQL por command.
+			MysqlParameter mySqlParameter = mySqlCommandChange.CreateParameter();
+			mySqlParameter.Parameter = "nombre";
+			mySqlParameter.Value = DateTime.Now.ToString();
+			mySqlCommandChange.Parameters.Add(mySqlParameter);
 			
-			MySqlDataReader mySqlDataReader2; //Creamos otro DataReader
-			mySqlDataReader2 = mySqlCommandChange.ExecuteNonQuery(); // ExecuteNonQuery ejecuta el Update
 			
-			while(mySqlDataReader.Read()){ //Leemos todas las filas
-				Console.WriteLine (mySqlDataReader.GetString (0) + ", " + mySqlDataReader.GetString (1));
-			}
+			//MySqlDataReader mySqlDataReader; //Creamos un DataReader
+			//mySqlDataReader = mySqlCommand.ExecuteReader(); // Nos devuelve un mySqlDataReader, lector de datos
 			
-			while(mySqlDataReader2.Read()){ //Leemos todas las filas
-				Console.WriteLine (mySqlDataReader2.GetString (0) + ", " + mySqlDataReader.GetString (1));
+			MySqlDataReader mySqlDataReaderChange; //Creamos otro DataReader
+			mySqlDataReaderChange = mySqlCommandChange.ExecuteNonQuery();
+			// ExecuteNonQuery ejecuta el Update
+			mySqlDataReaderChange.Close();
+			
+			//while(mySqlDataReader.Read()){ //Leemos todas las filas
+			//	Console.WriteLine (mySqlDataReader.GetString (0) + ", " + mySqlDataReader.GetString (1));
+			//}
+			
+			while(mySqlDataReaderChange.Read()){ //Leemos todas las filas
+				Console.WriteLine (mySqlDataReaderChange.GetString (0) + ", " + mySqlDataReaderChange.GetString (1));
 			}
 			
 
